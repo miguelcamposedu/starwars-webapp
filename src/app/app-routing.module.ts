1caa7f63081134525602f5485f4f67a03ffe9c58
Miguel Campos <camposmiguel@gmail.com>
2021-11-11 07:52:11 +0100

    basic app structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PeopleListComponent } from './pages/people-list/people-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'people', component: PeopleListComponent },
+  { path: '', pathMatch: 'full', redirectTo: '/people' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PeopleListComponent } from './pages/people-list/people-list.component';
+import { PersonItemComponent } from './components/person-item/person-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PeopleListComponent,
+    PersonItemComponent
   ],
   imports: [
     BrowserModule,
